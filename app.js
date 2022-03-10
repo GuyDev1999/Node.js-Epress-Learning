ad7087b8d4a9c62a0cc5710b8ad5b5cc88caedd6
Jetguy <lawbreakser@gmail.com>
2022-03-10 15:41:18 +0700

    add

--- app.js
+++ app.js
@@ -5,17 +5,17 @@ const morgan = require('morgan');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT;
 
 app.use(morgan('combined'));
 app.use(express.static(path.join(__dirname, "/public/")));
 
 app.get("/", (req,res) => {
 
-    res.send('Hello Jetguy DIA3');
+    res.send('Hello Jetguy Immortal');
 
 })
 
-app.listen(port, ()=>{
-    debug("Listerning on port" + chalk.green(" : " + port));
+app.listen(PORT, ()=>{
+    debug("Listerning on port" + chalk.red(" : " + PORT));
 })
