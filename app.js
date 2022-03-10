d8050c07dd5be6ec33d6f7ae86f3ff9d58c04625
Jetguy <lawbreakser@gmail.com>
2022-03-10 17:23:22 +0700

    update

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
-
+const productRouter = express.Router();
 const app = express();
 const PORT = process.env.PORT;
 
@@ -12,6 +12,12 @@ app.use(express.static(path.join(__dirname, "/public/")));
 
 app.set("views","./src/views");
 app.set("view engine", "ejs");
+
+productRouter.route("/").get((req,res) => {
+    res.send("Hello I'm Product");
+});
+
+app.use("/products", productRouter)
 
 app.get("/", (req,res) => {
 
